49ee2073ffcd0c30f516a4a97c1b4709de0748c9
IHVD <krisgalama@gmail.com>
2018-12-10 01:38:53 +0100

    working on teleportation. need to figure out the main camera, should probably move the entire OVRCameraRig instead.

---
+++ Assets/_Scripts/FadeController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class FadeController : MonoBehaviour {
+	public UnityEvent fadeDone;
+
+	public void FadeDoneHandler() {
+		fadeDone.Invoke();
+	}
+}
