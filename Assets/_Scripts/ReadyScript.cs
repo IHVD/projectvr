6a3a45baba04cbe50ea1f7f6439a812665ec3b09
NeonChicken <lcvandort@hotmail.com>
2018-12-16 16:36:14 +0100

    - Added ready button canvas (not interactable yet) - Changed scene lay-out - Orginized Hierarchy - Book is interactable

---
+++ Assets/_Scripts/ReadyScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadyScript : MonoBehaviour {
+
+	bool isReady;
+
+	public void clickedReady() {
+		bool isReady = true;
+	}
+}
