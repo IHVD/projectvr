0356ad63598889a5077e5c17910a95dbd87c0fc4
NeonChicken LAPTOP <lcvandort@hotmail.com>
2018-12-17 13:27:34 +0100

    Added ready script to disappear when clicked

--- Assets/_Scripts/ReadyScript.cs
+++ Assets/_Scripts/ReadyScript.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class ReadyScript : MonoBehaviour {
 
 	bool isReady;
+	public GameObject readyCanvas;
 
 	public void clickedReady() {
 		bool isReady = true;
+		readyCanvas.SetActive(false);
 	}
 }
