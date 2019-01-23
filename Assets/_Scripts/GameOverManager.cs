b7330cecf5183c165edd17e4ef8a461263e22286
YouriDaBomb <yourimulder96@outlook.com>
2019-01-23 19:09:13 +0100

    End phase canvas updated, added function for calling said canvas, added it to the labroom scene

--- Assets/_Scripts/GameOverManager.cs
+++ Assets/_Scripts/GameOverManager.cs
@@ -5,9 +5,21 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class GameOverManager : MonoBehaviour {
 
+	//variable for time 
+	public int ExperimentTimeLeft = 200;
+
+	public GameObject EndPhaseCanvas;
+
 	//acces to the score text 
 	public Text scoreValue;
 
+	void EndPhaseStart() {
+		if(ExperimentTimeLeft >= 0){
+			EndPhaseCanvas.SetActive(false);
+		}
+		else
+			EndPhaseCanvas.SetActive(true);		
+	}
 	// Use this for initialization
 	void Update () {
 		//set the text of the score
@@ -20,6 +32,5 @@ public class GameOverManager : MonoBehaviour {
 	//put on restart button so game resets properly
 	public void RestartGame(){
 		//Experiment.ScoreInstance.ResetGameVoidWeNeedToMake <--- resets score and puts player back in starting point, only puts back in starting point if the canvas stays in the same scene
-		//SceneManager.LoadScene("SceneWeNeedToLoadToRestartTheGame"); <--- this is only if we make the end screen in a new scene
 	}
 }
